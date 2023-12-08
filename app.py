7c998e3774c1a76eb771c5e1272aa6c52d6f57a6
yaziid14 <azharyazied@gmail.com>
2023-12-08 20:16:12 +0700

    Update1

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def admin():
     return render_template('admin.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=0000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
  
