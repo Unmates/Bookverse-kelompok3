077af4c74d310451a2a32422f0328a347affe5e2
Unmates <farellishaktibaihaqi@gmail.com>
2023-12-08 19:43:36 +0700

    tambah halaman admin

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=0000, debug=True)
  
