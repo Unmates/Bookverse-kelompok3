2f01c4573f338fe3c6ff523fd6f1eb6c9448cabd
rendrarya <rendrarya12345@gmail.com>
2023-12-09 03:24:28 +0800

    input buku page

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
 def admin():
     return render_template('admin.html')
 
+@app.route('/tambah')
+def tambah():
+    return render_template('tambahbuku.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
  
