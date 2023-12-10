ae98d1bf35c4c5090e86117e75b119e2e8da4c47
yaziid14 <azharyazied@gmail.com>
2023-12-11 05:11:14 +0700

    UpdateUser

--- app.py
+++ app.py
@@ -30,6 +30,14 @@ def tambah():
 def profile():
     return render_template('profile.html')
 
+@app.route('/favorite')
+def favorite():
+    return render_template('favorite.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
  
