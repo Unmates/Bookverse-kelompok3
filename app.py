42f5ff08b4f9c3d378f832f3c295114addae66aa
rendrarya <rendrarya12345@gmail.com>
2023-12-11 14:34:53 +0800

    repair commit from yazid

--- app.py
+++ app.py
@@ -26,6 +26,18 @@ def radmin():
 def tambah():
     return render_template('tambahbuku.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/favorite')
+def favorite():
+    return render_template('favorite.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
  
