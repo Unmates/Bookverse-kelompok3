560d5fa1fe89cadbf342d33ba479545281dea6ff
yaziid14 <azharyazied@gmail.com>
2023-12-14 01:21:09 +0700

    InputBuku

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from datetime import datetime
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+
+MONGODB_CONNECTION_STRING = "mongodb+srv://farelli:shakti@bookdb.nftj3pv.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(MONGODB_CONNECTION_STRING)
+db = client.testinput
 
 @app.route('/')
 def index():
@@ -24,6 +32,35 @@ def radmin():
 @app.route('/tambah')
 def tambah():
     return render_template('tambahbuku.html')
+
+@app.route("/tambahbuku", methods=["POST"])
+def tambahbuku():
+    judul_receive = request.form.get('judul_give')
+    deskripsi_receive = request.form.get('deskripsi_give')
+    harga_receive = request.form.get('harga_give')
+    kategori_receive = request.form.get('kategori_give')
+    bahasa_receive = request.form.get('bahasa_give')
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    file = request.files['gambar_give']
+    extension = file.filename.split('.')[-1]
+    filename = f'static/cover-{mytime}.{extension}'
+    file.save(filename)
+
+    doc = {
+        'Date': mytime,
+        'JudulBuku': judul_receive,
+        'Deskripsi': deskripsi_receive,
+        'Harga': harga_receive,
+        'Kategori': kategori_receive,
+        'Bahasa': bahasa_receive,
+        'Cover': filename
+    }
+
+    db.bukutest.insert_one(doc)
+    return jsonify({'msg':'Input Buku Berhasil!'})
 
 @app.route('/profile')
 def profile():
