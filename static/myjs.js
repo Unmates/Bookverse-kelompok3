6e456e3c6c65a5ec1300cee288551156f5e3bc71
Unmates <farellishaktibaihaqi@gmail.com>
2023-12-16 02:52:44 +0700

    add restirected role & jquery cdn

---
+++ static/myjs.js
@@ -0,0 +1,8 @@
+function check_admin(){
+    let role= $.cookie('role');
+    console.log(role)
+    if (role !== "admin") {
+        alert("You're not signed in as admin!");
+        window.location.replace("/login")
+    }
+}
